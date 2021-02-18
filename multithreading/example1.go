5710c06887168083873b14493dd9b7b9b228c679
Pavel <lwrahgqvjeapokr@gmail.com>
2021-02-19 00:47:34 +0200

    mulithreading

--- multithreading/example1.go
+++ multithreading/example1.go
@@ -1,17 +1,32 @@
 package main
 
-import ...
+import "fmt"
 
-type myString string 
+type myString string
 
-func (*myString) process (i int) {
-	fmt.Println(a: "process struct", i)
+func (*myString) process(i int) {
+	fmt.Println(i)
 }
 
-//---------//
-
-func process(i int)  {
-	
+func process(i int) {
+	fmt.Println(i)
 }
 
+func main() {
 
+	fmt.Println("start go routine")
+	go process(1)
+
+	go func() {
+		fmt.Println("anon")
+	}()
+
+	for i := 0; i < 10; i++ {
+		go process(i)
+	}
+
+	myStr := new(myString)
+	go myStr.process(1)
+
+	fmt.Scanln()
+}
